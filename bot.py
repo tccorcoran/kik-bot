873d81adb303bd70504626c1b4202d3fb7f2f601
Thomas Corcoran <chasecorcoran@gmail.com>
2016-06-27 13:14:12 -0700

    added envion vars for db connection

--- bot.py
+++ bot.py
@@ -6,12 +6,23 @@ from kik import KikApi,Configuration
 
 app = Flask(__name__)
 
+
+DATABASES = {
+    'default': {
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
+
 access_token = os.environ.get('WIT_SERVER_ACCESS_TOKEN')
 BOT_USERNAME = os.environ.get('BOT_USERNAME')
 BOT_API_KEY = os.environ.get('KIK_API_KEY')
 WEBHOOK = os.environ.get('WEBHOOK')
 kik = KikApi(BOT_USERNAME, BOT_API_KEY)
 kik.set_configuration(Configuration(webhook=WEBHOOK))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASES['default']['HOST']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
