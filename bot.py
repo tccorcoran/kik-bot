db9e4908234ac3186ca0ab4c1423cce3b5c41f80
Thomas Corcoran <chasecorcoran@gmail.com>
2016-06-27 15:11:25 -0700

    adjust DB config settings

--- bot.py
+++ bot.py
@@ -7,22 +7,20 @@ from kik import KikApi,Configuration
 app = Flask(__name__)
 
 
-DATABASES = {
-    'default': {
+DATABASE = {
         'NAME': os.environ['RDS_DB_NAME'],
         'USER': os.environ['RDS_USERNAME'],
         'PASSWORD': os.environ['RDS_PASSWORD'],
         'HOST': os.environ['RDS_HOSTNAME'],
         'PORT': os.environ['RDS_PORT'],
-    }
 }
-
+DB_URI = 'postgres://{NAME}:{PASSWORD}@{HOST}'.format(**DATABASE)
 access_token = os.environ.get('WIT_SERVER_ACCESS_TOKEN')
 BOT_USERNAME = os.environ.get('BOT_USERNAME')
 BOT_API_KEY = os.environ.get('KIK_API_KEY')
 WEBHOOK = os.environ.get('WEBHOOK')
 kik = KikApi(BOT_USERNAME, BOT_API_KEY)
 kik.set_configuration(Configuration(webhook=WEBHOOK))
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASES['default']['HOST']
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
